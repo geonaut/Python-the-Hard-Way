504647db2cc866c26e9364decdc1776425f40a61
Oli <oli@ddc.uk.net>
2017-04-26 16:15:15 +0100

    Second session

--- ex14.py
+++ ex14.py
@@ -1,10 +1,12 @@
 from sys import argv
-
+#argv again
 script, user_name = argv
+# defining the prompt
 prompt = '> '
-
+#Needs args to start
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
+#asking for input within the script
 print "Do you like me %s?" % user_name
 likes = raw_input(prompt)
 
@@ -13,7 +15,7 @@ lives = raw_input(prompt)
 
 print "What kind of computer do you have?"
 computer = raw_input(prompt)
-
+#Print it out as a block
 print """
 Alright, so you said %r about liking me.
 You live in %r.  Not sure where that is.
---
+++ ex18.py
@@ -0,0 +1,23 @@
+# this one is like your scripts with argv
+# args* tells Python to take all the arguments to the function and then put them in args as a list
+def print_two(*args):
+    arg1, arg2 = args
+    print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+    print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# this just takes one argument
+def print_one(arg1):
+    print "arg1: %r" % arg1
+
+# this one takes no arguments
+def print_none():
+    print "I got nothin'."
+
+
+print_two("Zed","Shaw")
+print_two_again("Zed","Shaw")
+print_one("First!")
+print_none()
